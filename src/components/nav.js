721a1d753248a2935c03b3fa78cb863e366a90d5
terribledevice <longlost@gmail.com>
2021-04-04 22:20:49 -0400

    first commit, getting react project setup

---
+++ src/components/nav.js
@@ -0,0 +1,36 @@
+<nav className="navbar">
+<div className="container">
+  <div className="navbar-brand">
+    <a className="navbar-item" href="#">
+      <img src="https://cdn.emk.dev/templates/bulma-logo-light.png" alt="Logo">
+    </a>
+    <span className="navbar-burger burger" data-target="navbarMenu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+  </div>
+  <div id="navbarMenu" className="navbar-menu">
+    <div className="navbar-end">
+      <a className="navbar-item is-active">
+        Home
+      </a>
+      <a className="navbar-item">
+        Examples
+      </a>
+      <a className="navbar-item">
+        Features
+      </a>
+      <a className="navbar-item">
+        Team
+      </a>
+      <a className="navbar-item">
+        Archives
+      </a>
+      <a className="navbar-item">
+        Help
+      </a>
+    </div>
+  </div>
+</div>
+</nav>
