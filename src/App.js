bf9dd95d0a6cc2eb24bd2e590073319b9327334b
terribledevice <longlost@gmail.com>
2021-04-10 13:31:34 -0400

    Working with nested destructuring now

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
       },
     ];
 
-  const [searchTerm, setSearchTerm] = useState(' ');
+  const [searchTerm, setSearchTerm] = useState('React');
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -38,7 +38,7 @@ const App = () => {
     <div className="App">
       <Nav />
         <div className="container">
-          <Search onSearch={handleSearch} />
+          <Search search={searchTerm} onSearch={handleSearch} />
           <hr/>
           <List list={searchedStories} />
         </div>
@@ -46,24 +46,35 @@ const App = () => {
   );
 };
 
-const Search = (props) => (
+const Search = ({search, onSearch}) => (
+ 
       <div>
-          <label htmlFor="search">Search: </label>
-          <input id="search" type="text" onChange={props.onSearch} />
+          <label htmlFor="search">Search:
+          </label>
+          <input 
+          id="search" 
+          type="text" 
+          value={search} 
+          onChange={onSearch}
+          />
       </div>
-);
+  );
 
-const List = props => 
-  props.list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
+
+const List = ({list}) => 
+  list.map(item => <Item key={item.objectID} item={item} />);
+    
+    const Item = ({item}) => (
+        <div>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+    );
+
 
   
 
