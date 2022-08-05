ab58a492e8a2b12393122112ad3ad8064f98c800
jnutterdev <hello@jnutterdev.com>
2022-08-04 23:03:42 -0400

    Added Tailwind to project, worked on button a little

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Logo from "./components/Images/pokedex.png";
 
+import Button from "./components/Button";
+
 function App() {
  
   return (
-    
+
   <div>
     <img src={Logo} alt="Pokédex" />
-    <h1>Pokédex</h1>;
+    <h1>Pokédex</h1>
+    <Button />
   </div>
   
   )
